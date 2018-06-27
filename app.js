6bdf7138b53fe1c983a6ac57f1afbecc52e04771
Jack Pickering <jackpickering@admins-air.home>
2018-06-27 14:43:58 +0100

    Added mongoose

--- app.js
+++ app.js
@@ -4,19 +4,22 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-// var indexRouter = require('./routes/index');
-// var usersRouter = require('./routes/users');
+// Require the connection to the database (mongoose)
+require('./app_server/models/db');
 
 var app = express();
 
-// view engine setup
+// Specify where the views are found
 app.set('views', path.join(__dirname, '/app_server/views'));
+// view engine setup
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+// Specify where the static files are found
 app.use(express.static(path.join(__dirname, 'public')));
 
 // require that the app sends requests to the routes folder (index.js)
---
+++ app_server/models/db.js
@@ -0,0 +1,51 @@
+var mongoose = require('mongoose');
+var gracefulShutdown;
+var dbURI = 'mongodb://localhost/iconemy';
+mongoose.connect(dbURI);
+
+// Events that will log to console when fired
+// Connection established
+mongoose.connection.on('connected', function () {
+ console.log('Mongoose connected to ' + dbURI);
+});
+
+// Error in connection
+mongoose.connection.on('error',function (err) {
+ console.log('Mongoose connection error: ' + err);
+});
+
+// Connection disabled
+mongoose.connection.on('disconnected', function () {
+ console.log('Mongoose disconnected');
+}); 
+
+// Graceful shutdown is necessary to shutdown redundant DB connections after the server shuts down/restarts
+var gracefulShutdown = function (msg, callback) {
+	// Closing connection is asynchronous so we wait until that is done then call the callback passed as param2
+	mongoose.connection.close(function () {
+		console.log('Mongoose disconnected through ' + msg);
+		callback();
+ 	});
+};
+
+// SIGUSR2 is the signal emitted by nodemon when the server restarts automatically
+process.once('SIGUSR2', function () {
+	// Call function with additional callback function as it is asynchronous
+	gracefulShutdown('nodemon restart', function () {
+		process.kill(process.pid, 'SIGUSR2');
+ 	});
+});
+
+// SIGINT is the signal emitted by node.js when the server shuts down
+process.on('SIGINT', function () {
+	gracefulShutdown('app termination', function () {
+		process.exit(0);
+ 	});
+});
+
+// SIGTERM is the signal emitted by heroku when the server shuts down
+process.on('SIGTERM', function() {
+	gracefulShutdown('Heroku app shutdown', function () {
+		process.exit(0);
+	});
+});
