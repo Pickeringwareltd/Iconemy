7f1ceb7a9ff79828f39bd460014d04c1a83c4053
Jack Pickering <jackpickering@admins-air.home>
2018-09-13 14:49:39 +0100

    Added config vars to the tracking module for KeenIO

--- tracking/tracking.js
+++ tracking/tracking.js
@@ -4,8 +4,8 @@ var KeenTracking = require('keen-tracking');
 
 // Configure a client instance
 var client = new KeenTracking({
-  projectId: '5b573abcc9e77c000175c9eb',
-  writeKey: 'A1082A66A7AAD875D36A37E2C59C1477BB9113C4D4F0B67CAED20C1BFBC9AF04E25176C22035FE335C19BCC37957B1982DAA2A1FFA98CFAACEE6940BD97B666E3EABAC8B146CDC3E651AA7DFA500A1F92226E537A599490F3CDBA126503F0F33'
+  projectId: process.env.KEEN_PROJECT_ID,
+  writeKey: process.env.KEEN_WRITE_KEY
 });
 
 // This is fired from the middleware used by both API and server, the function is stored in app.js
