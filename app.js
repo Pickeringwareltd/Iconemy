8e1fb8f35203efb0f852755d8a94383c9a372e5d
Jack Pickering <jackpickering@admins-air.home>
2018-09-18 15:23:54 +0100

    Added HSTS protection and force HTTPS on staging also

--- app.js
+++ app.js
@@ -26,7 +26,18 @@ var admin_routes = require('./app_admin/routes/index');
 
 var app = express();
 
+// Force the user to connect via HTTPS
 app.use(https.requireHTTPS);
+
+// Force the user to connect via HSTS (HTTP String Transport Security)
+app.use(function (req, res, next) {
+  var aYear = 60 * 60 * 24 * 365;
+  // Set the Strict Transport Security header for a year
+  // Also include subdomains
+  res.set('Strict-Transport-Security','max-age=' + aYear + ';includeSubdomains');
+
+  next();
+});
 
 // ******************************************* SESSION STORE ***********************************
 if (process.env.NODE_ENV !== 'production') {
--- config/https.js
+++ config/https.js
@@ -4,8 +4,15 @@ const errors = require('../add-ons/errors');
 
 module.exports.requireHTTPS = function(req, res, next) {
 	try{
+
+		var env = false;
+
+		if(process.env.NODE_ENV === "production" || process.env.NODE_ENV === "staging"){
+			env = true;
+		}
+
 		// The 'x-forwarded-proto' check is for Heroku
-		if (!req.secure && req.get('x-forwarded-proto') !== 'https' && process.env.NODE_ENV === "production") {
+		if (!req.secure && req.get('x-forwarded-proto') !== 'https' && env) {
 		  return res.redirect('https://' + req.get('host') + req.url);
 		}
 
