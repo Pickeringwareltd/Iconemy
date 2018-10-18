c749ffcc7f8c8ed2bf257819d59c320387debb69
Jack Pickering <jackpickering@Admins-MacBook-Air.local>
2018-10-18 09:36:08 +0100

    Added a verification for iconemy email

--- add-ons/tracking.js
+++ add-ons/tracking.js
@@ -2,17 +2,17 @@
 
 var KeenTracking = require('keen-tracking');
 
-// // Configure a client instance
-// const client = new KeenTracking({
-//   projectId: process.env['KEEN_PROJECT_ID'],
-//   writeKey: process.env['KEEN_WRITE_KEY']
-// });
-
 // Configure a client instance
 const client = new KeenTracking({
-  projectId: '5b573abcc9e77c000175c9eb',
-  writeKey: 'A1082A66A7AAD875D36A37E2C59C1477BB9113C4D4F0B67CAED20C1BFBC9AF04E25176C22035FE335C19BCC37957B1982DAA2A1FFA98CFAACEE6940BD97B666E3EABAC8B146CDC3E651AA7DFA500A1F92226E537A599490F3CDBA126503F0F33'
+  projectId: process.env.KEEN_PROJECT_ID,
+  writeKey: process.env.KEEN_WRITE_KEY
 });
+
+// // Configure a client instance
+// const client = new KeenTracking({
+//   projectId: '5b573abcc9e77c000175c9eb',
+//   writeKey: 'A1082A66A7AAD875D36A37E2C59C1477BB9113C4D4F0B67CAED20C1BFBC9AF04E25176C22035FE335C19BCC37957B1982DAA2A1FFA98CFAACEE6940BD97B666E3EABAC8B146CDC3E651AA7DFA500A1F92226E537A599490F3CDBA126503F0F33'
+// });
 
 // This is fired from the middleware used by both API and server, the function is stored in app.js
 module.exports.view = function(req, res, next){
@@ -125,7 +125,7 @@ module.exports.crowdsaleview = function(req, res, project, sale){
 		}
 
 		// Record an event
-		client.recordEvent('crowdsaleviews', {
+		client.recordEvent('crowdsaleviewingexample', {
 		  path: req.path,
 		  user: req.headers['user-agent'],
 		  mobile: mobile,
@@ -134,8 +134,7 @@ module.exports.crowdsaleview = function(req, res, project, sale){
 		  refer: req.headers.referer
 		});
 
-		console.log( process.env['KEEN_PROJECT_ID'] );
-		console.log( process.env['KEEN_WRITE_KEY'] );
+		console.log(process.env.KEEN_PROJECT_ID);
 
 	} catch(e) {
 		console.log('Error on tracking.js/crowdsaleview: ' + e);
